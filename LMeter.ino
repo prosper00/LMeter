8d20e1e0c698e143e834a300c46fd3549be5c8a0
prosper00 <brad.roy@gmail.com>
2021-05-12 09:34:59 -0600

    UI tweaks

--- LMeter.ino
+++ LMeter.ino
@@ -1,7 +1,7 @@
 /*
- LMeter.
+  LMeter.
 
- To do - write stuff here.
+  To do - write stuff here.
 */
 #include <LiquidCrystal.h>
 
@@ -12,7 +12,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  
+
   //on 328p, OC1A function is tied to pin PB1 (D9)
   pinMode( D9, OUTPUT );
 
@@ -21,9 +21,13 @@ void setup() {
   Serial.begin(115200);
   lcd.begin(16, 2);
   delay(100); // wait for the LCD to init
-  
+  lcd.setCursor(0, 0); lcd.print("uH: ");
+  lcd.setCursor(0, 1); lcd.print("mV: ");
+
   //TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 65kHz @ 32MHz on Tim2 channels
 
+  //Speed up ADC. On AVR, comment out the following, or adjust the prescaler value to something higher. (64?)
+  //ADCSRA = ((0 << ADPS0) | (0 << ADPS1) | (1 << ADPS2));   // ADC prescaler to 16, ADC Clk to 2MHz / 133MSpS
   setFrequency(200000);
 }
 
@@ -31,34 +35,34 @@ void setup() {
 void setFrequency(uint32_t freq)
 {
   TCCR1A &= ~(1 << COM1A0); //turn off output
-  if (freq==0) return;
+  if (freq == 0) return;
 
-  uint32_t requiredDivisor = (F_CPU/2)/freq;
+  uint32_t requiredDivisor = (F_CPU / 2) / freq;
 
   uint16_t prescalerVal;
   uint8_t prescalerBits;
-  if (requiredDivisor < 65536UL){
+  if (requiredDivisor < 65536UL) {
     prescalerVal = 1;
     prescalerBits = 1;
   }
-  else if (requiredDivisor < 8 * 65536UL){
+  else if (requiredDivisor < 8 * 65536UL) {
     prescalerVal = 8;
     prescalerBits = 2;
   }
-  else if (requiredDivisor < 64 * 65536UL){
+  else if (requiredDivisor < 64 * 65536UL) {
     prescalerVal = 64;
     prescalerBits = 3;
   }
-  else if (requiredDivisor < 256 * 65536UL){
+  else if (requiredDivisor < 256 * 65536UL) {
     prescalerVal = 256;
     prescalerBits = 4;
   }
-  else{
+  else {
     prescalerVal = 1024;
     prescalerBits = 5;
   }
 
-  uint16_t top = ((requiredDivisor + (prescalerVal/2))/prescalerVal) - 1;
+  uint16_t top = ((requiredDivisor + (prescalerVal / 2)) / prescalerVal) - 1;
   TCCR1A = 0;
   TCCR1B = (1 << WGM12) | prescalerBits;
   TCCR1C = 0;
@@ -66,38 +70,41 @@ void setFrequency(uint32_t freq)
 
   //turn on output OCR1 (PB1/D9)
   TCNT1H = 0;
-  TCNT1L = 0;  
+  TCNT1L = 0;
   TCCR1A |= (1 << COM1A0);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  
-  
+
+
   //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   //delay(900);                       // wait for a second
   //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  
+
   delay(300);                       // wait for the LCD
-    
-  lcd.setCursor(0,0);
+
+  uint16_t voltage = ReadInductor(); //== reading * vref / 2^12-bit == 4096mV/4096 == vref/1
+  lcd.setCursor(5, 0);
   lcd.print("        ");
-  lcd.setCursor(0,0);
-  lcd.print(curveFit(ReadInductor()));
+  lcd.setCursor(5, 0);
+  lcd.print(curveFit(voltage));
+  lcd.setCursor(5, 1);
+  lcd.print(voltage);
 }
 
-uint16_t ReadInductor(void){
+uint16_t ReadInductor(void) {
   uint32_t runningtotal = 0;
-  for(int i=0; i<30; i++) runningtotal+=analogRead(A0);
-  return (uint16_t)(runningtotal/30);
+  for (int i = 0; i < 30; i++) runningtotal += analogRead(A0);
+  return (uint16_t)(runningtotal / 30);
 }
 
 /*curve-fitting analysis from several hundred inductors tested shows that
- * y = 21337840*e^(-1*(x+12785.47)^2/(2*2927.016^2))
- * (gaussian curve)
- * for all values of x from 10 to 470, give or take a tiny bit
- */
-float curveFit(uint16_t voltage){
-  #define euler 2.71828
-  return 21337840*pow(euler, (-1*pow((voltage+12785.47),2)/(2*pow(2927.016,2))));
+   y = 21337840*e^(-1*(x+12785.47)^2/(2*2927.016^2))
+   (gaussian curve)
+   for all values of x from 10 to 470, give or take a tiny bit
+*/
+float curveFit(uint16_t voltage) {
+#define euler 2.71828
+  return 21337840 * pow(euler, (-1 * pow((voltage + 12785.47), 2) / (2 * pow(2927.016, 2))));
 }
